5c0a19d0d2e7cea00108b0954b31f6c868b1d1b5
plitwin <pawel.litwin83@gmail.com>
2022-12-12 21:55:33 +0100

    First commit of the empty project with plan of the interface and classes

---
+++ Plan/CashFlowApp.mm
@@ -0,0 +1,412 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1669926493982" ID="ID_939213368" MODIFIED="1669926606270" TEXT="CashFlow App">
+<node CREATED="1669926626922" ID="ID_637563570" MODIFIED="1669926631359" POSITION="right" TEXT="Menu G&#x142;&#xf3;wne">
+<node CREATED="1669926648820" HGAP="21" ID="ID_78161855" MODIFIED="1669929011812" TEXT="1. Rejestracja" VSHIFT="-15">
+<node CREATED="1669926675772" ID="ID_1529046949" MODIFIED="1669926770496" TEXT="Podaj Imi&#x119;">
+<node CREATED="1669926684464" ID="ID_187478028" MODIFIED="1669926690736" TEXT="Podaj nazwisko">
+<node CREATED="1669926691835" ID="ID_1490467296" MODIFIED="1669926695504" TEXT="Podaj login">
+<node CREATED="1669926775070" ID="ID_660193222" MODIFIED="1669926783090" TEXT="Czy login jest wolny?">
+<node CREATED="1669926783372" ID="ID_1233708017" MODIFIED="1669926785535" TEXT="TAK">
+<node CREATED="1669926786825" ID="ID_519625507" MODIFIED="1669926790754" TEXT="Podaj has&#x142;o">
+<node CREATED="1669926837637" ID="ID_1703784907" MODIFIED="1669926846178" TEXT="Zapisz u&#x17c;ytkownika w pliku XML">
+<node CREATED="1669926939584" ID="ID_1530577098" MODIFIED="1669929190269">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Informacja: Nowe konto zosta&#322;o utworzone.
+    </p>
+    <p>
+      Czy chcesz si&#281; zalogowa&#263;?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1669926959712" ID="ID_1546099922" MODIFIED="1669926961580" TEXT="TAK">
+<node CREATED="1669926974159" ID="ID_1647866640" MODIFIED="1669926981030" TEXT="Wy&#x15b;wietl menu logowanie"/>
+</node>
+<node CREATED="1669926963170" ID="ID_498600568" MODIFIED="1669926964617" TEXT="NIE">
+<node CREATED="1669926967387" ID="ID_442378" MODIFIED="1669926972727" TEXT="Powr&#xf3;t do menu g&#x142;&#xf3;wnego"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1669926796345" ID="ID_59318113" MODIFIED="1669926802159" TEXT="NIE">
+<node CREATED="1669926802913" ID="ID_515529429" MODIFIED="1669929235177">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Informacja: login jest zaj&#281;ty.
+    </p>
+    <p>
+      Podaj inny login:
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1669927011533" ID="ID_293295730" MODIFIED="1669927018456" TEXT="Wy&#x15b;wietl ponownie monit o login"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1669926657163" ID="ID_831654310" MODIFIED="1669927171157" TEXT="2. Logowanie">
+<node CREATED="1669926723667" ID="ID_1081843615" MODIFIED="1669926728471" TEXT="Podaj login">
+<node CREATED="1669926729920" ID="ID_540034022" MODIFIED="1669926732987" TEXT="Podaj has&#x142;o">
+<node CREATED="1669926747569" ID="ID_1942065969" MODIFIED="1669929207955">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Czy dane logowania
+    </p>
+    <p>
+      s&#261; poprawne?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1669926757333" ID="ID_1743155403" MODIFIED="1669926759356" TEXT="TAK">
+<node CREATED="1669927025551" ID="ID_1800471525" MODIFIED="1669929158204">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Wczytaj do&#160;&#160;pami&#281;ci wydatki
+    </p>
+    <p>
+      i przychody zalogowanego&#160;&#160;u&#380;ytkownika
+    </p>
+    <p>
+      i wy&#347;wietl menu u&#380;ytkownika
+    </p>
+  </body>
+</html>
+</richcontent>
+<arrowlink DESTINATION="ID_1800471525" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1559077406" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1800471525" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1559077406" SOURCE="ID_1800471525" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1669927314806" ID="ID_1661921939" MODIFIED="1669927324423" TEXT="1. Dodaj przych&#xf3;d">
+<node CREATED="1669927441004" ID="ID_1135934698" MODIFIED="1669929241499">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Czy chcesz doda&#263; przych&#243;d
+    </p>
+    <p>
+      z dzisiejsz&#261; dat&#261;?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1669927464156" ID="ID_777617603" MODIFIED="1669927465580" TEXT="TAK">
+<node CREATED="1669927897889" ID="ID_1908553047" MODIFIED="1669927913381" TEXT="Przejd&#x17a; do &quot;Podaj kategori&#x119; przychodu"/>
+</node>
+<node CREATED="1669927467050" ID="ID_160050947" MODIFIED="1669927467887" TEXT="NIE">
+<node CREATED="1669927471920" ID="ID_150016424" MODIFIED="1669927489098" TEXT="Podaj dat&#x119;, kt&#xf3;rej dotyczy przych&#xf3;d">
+<node CREATED="1669927493521" ID="ID_1303334103" MODIFIED="1669929098654">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Czy data zawiera si&#281; w przedziale
+    </p>
+    <p>
+      01.01.2000 - ostatni dzie&#324; obecnego miesi&#261;ca?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1669927522922" ID="ID_1316476791" MODIFIED="1669927525479" TEXT="TAK">
+<node CREATED="1669927563344" ID="ID_1567863738" MODIFIED="1669927892777" TEXT="Podaj kategori&#x119; przychodu">
+<node CREATED="1669927625595" ID="ID_678868684" MODIFIED="1669928265517" TEXT="Podaj kwot&#x119; wydatku">
+<node CREATED="1669927667760" ID="ID_699455619" MODIFIED="1669929086957">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Czy warto&#347;&#263; zosta&#322;a zapisana
+    </p>
+    <p>
+      z przecinkiem zamiast kropki?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1669927684489" ID="ID_1125709450" MODIFIED="1669927685429" TEXT="TAK">
+<node CREATED="1669927760726" ID="ID_1768361828" MODIFIED="1669927767930" TEXT="Zamie&#x144; przecinek  na kropk&#x119;">
+<node CREATED="1669927804543" ID="ID_1268049029" MODIFIED="1669927815278" TEXT="Zapisz nowy przych&#xf3;d w pami&#x119;ci i w pliku XML"/>
+</node>
+</node>
+<node CREATED="1669927688038" ID="ID_943217403" MODIFIED="1669927690871" TEXT="NIE">
+<node CREATED="1669927770709" ID="ID_1801957249" MODIFIED="1669927786767" TEXT="Zapisz nowy przych&#xf3;d w pami&#x119;ci i w pliku XML"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1669927527058" ID="ID_1794073831" MODIFIED="1669927527841" TEXT="NIE">
+<node CREATED="1669927529178" ID="ID_514174118" MODIFIED="1669929079198">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Informacja: &quot;Nieprawid&#322;owa data.
+    </p>
+    <p>
+      Data musi si&#281; zawiera&#263; w przedziale
+    </p>
+    <p>
+      01.01.2000 - koniec bie&#380;&#261;cego miesi&#261;ca (data)
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1669927657204" ID="ID_925975401" MODIFIED="1669927661712" TEXT="Ponowny monit o dat&#x119;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1669927327326" ID="ID_1863491897" MODIFIED="1669927338815" TEXT="2. Dodaj wydatek">
+<node CREATED="1669927441004" ID="ID_1402782541" MODIFIED="1669929246432">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Czy chcesz doda&#263; wydatek
+    </p>
+    <p>
+      z dzisiejsz&#261; dat&#261;?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1669927464156" ID="ID_590844767" MODIFIED="1669927465580" TEXT="TAK">
+<node CREATED="1669927897889" ID="ID_140961098" MODIFIED="1669928238173" TEXT="Przejd&#x17a; do &quot;Podaj kategori&#x119; wydatku&quot;"/>
+</node>
+<node CREATED="1669927467050" ID="ID_1138145873" MODIFIED="1669927467887" TEXT="NIE">
+<node CREATED="1669927471920" ID="ID_1952000019" MODIFIED="1669928229519" TEXT="Podaj dat&#x119;, kt&#xf3;rej dotyczy wydatek">
+<node CREATED="1669927493521" ID="ID_544962252" MODIFIED="1669929111113">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Czy data zawiera si&#281; w przedziale
+    </p>
+    <p>
+      01.01.2000 - ostatni dzie&#324; obecnego miesi&#261;ca?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1669927522922" ID="ID_1999542026" MODIFIED="1669927525479" TEXT="TAK">
+<node CREATED="1669927563344" ID="ID_621232303" MODIFIED="1669928245930" TEXT="Podaj kategori&#x119; wydatku">
+<node CREATED="1669927625595" ID="ID_755558500" MODIFIED="1669928258644" TEXT="Podaj kwot&#x119; wydatku">
+<node CREATED="1669927667760" ID="ID_1539568657" MODIFIED="1669929042476">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Czy warto&#347;&#263; zosta&#322;a zapisana
+    </p>
+    <p>
+      z przecinkiem zamiast kropki?
+    </p>
+  </body>
+</html>
+</richcontent>
+<arrowlink DESTINATION="ID_1539568657" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_326829549" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1539568657" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_326829549" SOURCE="ID_1539568657" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1669927684489" ID="ID_1198826968" MODIFIED="1669927685429" TEXT="TAK">
+<node CREATED="1669927760726" ID="ID_532197956" MODIFIED="1669927767930" TEXT="Zamie&#x144; przecinek  na kropk&#x119;">
+<node CREATED="1669927804543" ID="ID_326189874" MODIFIED="1669928278687" TEXT="Zapisz nowy wydatek w pami&#x119;ci i w pliku XML"/>
+</node>
+</node>
+<node CREATED="1669927688038" ID="ID_765734089" MODIFIED="1669927690871" TEXT="NIE">
+<node CREATED="1669927770709" ID="ID_1564061159" MODIFIED="1669929052179">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Zapisz nowy wydatek
+    </p>
+    <p>
+      w pami&#281;ci i w pliku XML
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1669928538648" ID="ID_1075822274" MODIFIED="1669928554878" TEXT="Informacja: &quot;Nowy wydatek zosta&#x142;  zapisany&quot;">
+<node CREATED="1669928563094" ID="ID_167005274" MODIFIED="1669928679287" TEXT="Wy&#x15b;wietl menu Dalsze kroki">
+<node CREATED="1669928589459" ID="ID_1597918991" MODIFIED="1669928692035" TEXT="1. Dodaj kolejny wydatek z t&#x105; sam&#x105;  dat&#x105;">
+<node CREATED="1669928599159" ID="ID_633180518" MODIFIED="1669928611508" TEXT="Id&#x17a; do:  Podaj kategori&#x119;"/>
+</node>
+<node CREATED="1669928594608" ID="ID_456705491" MODIFIED="1669928702701" TEXT="2. Dodaj wydatek z inn&#x105; dat&#x105;">
+<node CREATED="1669928707445" ID="ID_929947994" MODIFIED="1669928716081" TEXT="Id&#x17a; do &quot;Podaj dat&#x119;&quot;"/>
+</node>
+<node CREATED="1669928721551" ID="ID_752541933" MODIFIED="1669928731093" TEXT="3. Wr&#xf3;&#x107; do menu u&#x17c;ytkownika"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1669927527058" ID="ID_1047050447" MODIFIED="1669927527841" TEXT="NIE">
+<node CREATED="1669927529178" ID="ID_1814367492" MODIFIED="1669929134212" TEXT="Informacja: &quot;Nieprawid&#x142;owa data.&#xa;Data musi si&#x119; zawiera&#x107; w przedziale&#xa;01.01.2000 - koniec bie&#x17c;&#x105;cego miesi&#x105;ca (data)">
+<node CREATED="1669927657204" ID="ID_1664223106" MODIFIED="1669927661712" TEXT="Ponowny monit o dat&#x119;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1669927349498" ID="ID_1271198050" MODIFIED="1669927358725" TEXT="3. Bilans z bie&#x17c;&#x105;cego miesi&#x105;ca">
+<node CREATED="1669928807568" ID="ID_433566844" MODIFIED="1669928827400" TEXT="Wy&#x15b;wietl bilans z bie&#x17c;&#x105;cego  miesi&#x105;ca">
+<node CREATED="1669928563094" ID="ID_1240264295" MODIFIED="1669928679287" TEXT="Wy&#x15b;wietl menu Dalsze kroki">
+<node CREATED="1669928589459" ID="ID_1241479460" MODIFIED="1669928902729" TEXT="1. Wy&#x15b;wietl bilans z poprzedniego miesi&#x105;ca"/>
+<node CREATED="1669928594608" ID="ID_90489296" MODIFIED="1669928924964" TEXT="2. Wy&#x15b;wietl bilans z wybranego okresu">
+<node CREATED="1669928707445" ID="ID_1794591588" MODIFIED="1669928716081" TEXT="Id&#x17a; do &quot;Podaj dat&#x119;&quot;"/>
+</node>
+<node CREATED="1669928721551" ID="ID_1378443341" MODIFIED="1669928731093" TEXT="3. Wr&#xf3;&#x107; do menu u&#x17c;ytkownika"/>
+</node>
+</node>
+</node>
+<node CREATED="1669927365819" ID="ID_511927403" MODIFIED="1669927373667" TEXT="4. Bilans z poprzedniego miesi&#x105;ca">
+<node CREATED="1669928830120" ID="ID_1440372712" MODIFIED="1669928838593" TEXT="Wy&#x15b;wietl bilans z poprzedniego miesi&#x105;ca">
+<node CREATED="1669928563094" ID="ID_178907314" MODIFIED="1669928679287" TEXT="Wy&#x15b;wietl menu Dalsze kroki">
+<node CREATED="1669928589459" ID="ID_1075161332" MODIFIED="1669928947007" TEXT="1. Wy&#x15b;wietl bilans z bie&#x17c;&#x105;cego miesi&#x105;ca"/>
+<node CREATED="1669928594608" ID="ID_1512078006" MODIFIED="1669928924964" TEXT="2. Wy&#x15b;wietl bilans z wybranego okresu">
+<node CREATED="1669928707445" ID="ID_536959312" MODIFIED="1669928716081" TEXT="Id&#x17a; do &quot;Podaj dat&#x119;&quot;"/>
+</node>
+<node CREATED="1669928721551" ID="ID_400892690" MODIFIED="1669928731093" TEXT="3. Wr&#xf3;&#x107; do menu u&#x17c;ytkownika"/>
+</node>
+</node>
+</node>
+<node CREATED="1669927379242" ID="ID_1668165106" MODIFIED="1669927390082" TEXT="5. Bilans z wybranego okresy">
+<node CREATED="1669928351451" ID="ID_832779621" MODIFIED="1669928358110" TEXT="Podaj dat&#x119; pocz&#x105;tkow&#x105;">
+<node CREATED="1669928359305" ID="ID_284613309" MODIFIED="1669928365336" TEXT="Podaj dat&#x119; ko&#x144;cow&#x105;">
+<node CREATED="1669928366493" ID="ID_739413344" MODIFIED="1669928378965" TEXT="Czy data ko&#x144;cowa &gt;= Data ko&#x144;cowa">
+<node CREATED="1669928380601" ID="ID_152725343" MODIFIED="1669928384140" TEXT="TAK">
+<node CREATED="1669928427144" ID="ID_652849214" MODIFIED="1669928441488" TEXT="Wy&#x15b;wietl bilans wybranego okresu">
+<node CREATED="1669928563094" ID="ID_156306136" MODIFIED="1669928679287" TEXT="Wy&#x15b;wietl menu Dalsze kroki">
+<node CREATED="1669928589459" ID="ID_388920160" MODIFIED="1669928947007" TEXT="1. Wy&#x15b;wietl bilans z bie&#x17c;&#x105;cego miesi&#x105;ca"/>
+<node CREATED="1669928594608" ID="ID_984317748" MODIFIED="1669928979675" TEXT="2. Wy&#x15b;wietl bilans z poprzeniego miesi&#x105;ca"/>
+<node CREATED="1669928721551" ID="ID_1148552975" MODIFIED="1669928731093" TEXT="3. Wr&#xf3;&#x107; do menu u&#x17c;ytkownika"/>
+</node>
+</node>
+</node>
+<node CREATED="1669928385432" ID="ID_418431873" MODIFIED="1669928386336" TEXT="NIE">
+<node CREATED="1669928387868" ID="ID_1013367157" MODIFIED="1669928425466" TEXT="Informacja:  &quot;Data pocz&#x105;tkowa musi by&#x107; wcze&#x15b;niejsza ni&#x17c; ko&#x144;cowa&quot;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1669927397986" ID="ID_1183383299" MODIFIED="1669927404463" TEXT="6. Zmie&#x144; has&#x142;o">
+<node CREATED="1669928446860" ID="ID_745802536" MODIFIED="1669928452327" TEXT="Podaj obecne has&#x142;o">
+<node CREATED="1669928453282" ID="ID_483617390" MODIFIED="1669928459050" TEXT="Czy has&#x142;o poprawne?">
+<node CREATED="1669928459887" ID="ID_1713186642" MODIFIED="1669928461427" TEXT="TAK">
+<node CREATED="1669928467296" ID="ID_201708956" MODIFIED="1669928472535" TEXT="Podaj nowe has&#x142;o">
+<node CREATED="1669928473689" ID="ID_1968862809" MODIFIED="1669928478218" TEXT="Powt&#xf3;rz nowe has&#x142;o">
+<node CREATED="1669928479437" ID="ID_690572123" MODIFIED="1669928496601" TEXT="Czy nowe i powt&#xf3;rzone has&#x142;a s&#x105; takie same?">
+<node CREATED="1669928499362" ID="ID_190577519" MODIFIED="1669928501201" TEXT="TAK">
+<node CREATED="1669928502675" ID="ID_1666751645" MODIFIED="1669928510016" TEXT="Zmie&#x144; has&#x142;o w pami&#x119;ci i w pliku">
+<node CREATED="1669928518189" ID="ID_651656990" MODIFIED="1669928523079" TEXT="Wy&#x15b;wietl menu u&#x17c;ytkownika"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1669928462590" ID="ID_533342239" MODIFIED="1669928463318" TEXT="NIE">
+<node CREATED="1669928752315" ID="ID_481851265" MODIFIED="1669928779153" TEXT="Informacja: &quot;Aby zmieni&#x107; has&#x142;o nale&#x17c;y poda&#x107; najpierw obecnie ustawione|">
+<node CREATED="1669928782112" ID="ID_1170765167" MODIFIED="1669928789257" TEXT="Powr&#xf3;t do menu u&#x17c;ytkownika"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1669927420971" ID="ID_605130480" MODIFIED="1669927426072" TEXT="0. Wyloguj si&#x119;"/>
+</node>
+</node>
+<node CREATED="1669926760952" HGAP="7" ID="ID_489509322" MODIFIED="1669929182157" TEXT="NIE" VSHIFT="46">
+<node CREATED="1669927125287" ID="ID_1120194434" MODIFIED="1669929173925">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Informacja: &quot;Nieprawid&#322;owe dane logowania.
+    </p>
+    <p>
+      Spr&#243;b&#243;j ponownie&quot;
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1669927148080" ID="ID_415631637" MODIFIED="1669927151362" TEXT="Monit o login"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1660019900690" HGAP="24" ID="ID_924622472" MODIFIED="1669927412908" TEXT="0. Zako&#x144;cz program" VSHIFT="79">
+<node CREATED="1660249907334" ID="ID_191498898" MODIFIED="1669929217294">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Zapytaj czy na pewno
+    </p>
+    <p>
+      zako&#324;czy&#263; program?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1660249948591" HGAP="23" ID="ID_1197886129" MODIFIED="1660554286367" TEXT="Je&#x15b;li Tak - wyjd&#x17a; z programu" VSHIFT="-26"/>
+<node CREATED="1660249968135" HGAP="21" ID="ID_1300152060" MODIFIED="1660554289763" TEXT="Je&#x15b;li nie - powr&#xf3;&#x107; do menu g&#x142;&#xf3;wnego" VSHIFT="27"/>
+</node>
+</node>
+</node>
+</node>
+</map>
