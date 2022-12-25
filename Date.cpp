72413ecb3512f8561ded168def940a52f2b9f97e
plitwin <pawel.litwin83@gmail.com>
2022-12-25 16:43:46 +0100

    properties added to class Date and method getNumberOfDaysInMonth implemented

--- Date.cpp
+++ Date.cpp
@@ -32,3 +32,19 @@ int Date:: getCurrentDay() {
     int currentDay = pTInfo->tm_mday;
     return currentDay;
 }
+
+int  Date::getNumberOfDaysInMonth(int month, int year) {
+    //leap year condition, if month is 2
+    if( month == 2) {
+        if((year%400==0) || (year%4==0 && year%100!=0))
+            return 29;
+        else
+            return 28;
+    }
+    //months which has 31 days
+    else if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8
+            ||month == 10 || month==12)
+        return 31;
+    else
+        return 30;
+}
--- Date.h
+++ Date.h
@@ -10,11 +10,25 @@ using namespace std;
 
 class Date {
     string currentDate;
+    int currentDay;
+    int currentMonth;
+    int currentYear;
+    int currentMonthLength;
 
     int getCurrentDay();
     int getCurrentMonth();
     int getCurrentYear();
+    int daysInCurrentMonth();
+    int getNumberOfDaysInMonth(int month, int year);
+
 public:
+    Date() {
+        currentDate = determineCurrentDate();
+        currentDay = getCurrentDay();
+        currentMonth = getCurrentMonth();
+        currentYear = getCurrentYear();
+        currentMonthLength = getNumberOfDaysInMonth(currentMonth, currentYear);
+    };
     string determineCurrentDate();
 
 };
